2f92e01622958ad16454035cd884f7233e1c2a73
Bobiev Nodir <bobievnodir@yandex-team.ru>
2023-03-23 23:24:33 +0300

    remove sverdle

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -16,17 +16,14 @@
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
-		<ul>
+		<!-- <ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="{base}/">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="{base}/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="{base}/sverdle">Sverdle</a>
-			</li>
-		</ul>
+		</ul> -->
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+	import {base} from "$app/paths"
+
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
@@ -19,8 +24,5 @@
 		the devtools network panel and reloading.
 	</p>
 
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+	
 </div>
